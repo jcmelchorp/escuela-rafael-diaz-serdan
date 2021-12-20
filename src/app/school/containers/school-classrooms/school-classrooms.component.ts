fac29c0164cafe84fc8727346c67b51cb8999a12
Julio César Melchor Pinto <jcmelchorp@gmail.com>
2021-12-19 22:17:10 -0600

    print classrooms list

--- src/app/school/containers/school-classrooms/school-classrooms.component.ts
+++ src/app/school/containers/school-classrooms/school-classrooms.component.ts
@@ -13,7 +13,10 @@ import { heightReveal } from '@rds-shared/animations/fade-in.animation';
 import { AccountsEntityService } from '@rds-store/accounts/accounts-entity.service';
 import { map, mergeMap, switchMap } from 'rxjs/operators';
 import { User } from '@rds-auth/models/user.model';
-
+import * as pdfFonts from "pdfmake/build/vfs_fonts"; // fonts provided for pdfmake
+import pdfMake from "pdfmake/build/pdfmake";
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-school-classrooms',
   templateUrl: './school-classrooms.component.html',
@@ -46,7 +49,221 @@ export class SchoolClassroomsComponent implements OnInit {
   }
   assignCoursesToClassroom() { }
   deliveryDocument() { }
-  saveFile() { }
+  async classroomToPDF(classroom: SchoolClassroom) {
+    console.log(classroom);
+    const buildTableBody = (data, columns) => {
+      var body = [];
+      //body.push(columns);
+      data.forEach((row) => {
+        var dataRow = [];
+        columns.forEach((column) => {
+          dataRow.push(row[column]);
+        });
+
+        body.push(dataRow);
+      });
+
+      return body;
+    }
+    const getBase64ImageFromURL = (url) => {
+      return new Promise((resolve, reject) => {
+        var img = new Image();
+        img.setAttribute("crossOrigin", "anonymous");
+        img.onload = () => {
+          var canvas = document.createElement("canvas");
+          canvas.width = img.width;
+          canvas.height = img.height;
+          var ctx = canvas.getContext("2d");
+          ctx.drawImage(img, 0, 0);
+          var dataURL = canvas.toDataURL("image/png");
+          resolve(dataURL);
+        };
+        img.onerror = error => {
+          reject(error);
+        };
+        img.src = url;
+      });
+    }
+
+    var docDefinition = {
+      header: {
+        margin: 40,
+        columns: [
+          {
+            // usually you would use a dataUri instead of the name for client-side printing
+            // sampleImage.jpg however works inside playground so you can play with it
+            image: await getBase64ImageFromURL(
+              '/assets/images/rds-newlogo-transparent.png'),
+            width: 95
+          },
+
+        ]
+      }, content: [
+        {
+          text: 'Escuela Rafael Díaz Serdán',
+          fontSize: 20,
+          alignment: 'center',
+          color: '#0060a0',
+          bold: true,
+        },
+
+        {
+          text: 'Lista de grupo',
+          fontSize: 16,
+          bold: false,
+          alignment: 'center',
+        },
+        {
+          text: `Grado: ${this.levels[classroom.grade]}`,
+          alignment: 'center',
+          bold: true
+        },
+        {
+          text: `Ciclo escolar: ${this.cycles[classroom.cycle]}`,
+          alignment: 'center'
+        },
+        {
+          text: `Fecha de consulta: ${new Date().toLocaleString()}`,
+          alignment: 'center'
+        },
+        {
+          margin: [0, 30],
+          text: 'Lista de Alumnos',
+          fontSize: 14,
+          bold: false,
+          alignment: 'left',
+        },
+        {
+          fontSize: 8,
+          layout: {
+            hLineWidth: (i, node) => { return (i === 0 || i === 1 || i === -1) ? 1 : 0; },
+            vLineWidth: (i, node) => { return (i === 0 || i === 1 || i === 2 || i === 3 || i === 4 || i === 5 || i === 6) ? 1 : 0; },
+            hLineColor: (i, node) => { return (i === 0 || i === -1) ? '#101010' : '#0060a0'; },
+            vLineColor: (i, node) => { return (i === 0 || i === 1 || i === 2 || i === 3 || i === 4 || i === 5 || i === 6) ? '#080808' : '#ffffff' },
+            paddingBottom: (i, node) => {
+              switch (i) {
+                case 0:
+                  return 0;
+                case 1:
+                  return 0;
+                default:
+                  return 0;
+              }
+            },
+            paddingTop: (i, node) => {
+              switch (i) {
+                case 0:
+                  return 0;
+                case 1:
+                  return 0;
+                default:
+                  return 0;
+              }
+            }
+          },
+          table: {
+            headerRows: 0,
+            widths: ['auto', 'auto', '*', 'auto', 'auto', 'auto'],
+            //heights: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto'],
+            alignment: 'center',
+            body: [
+              [
+                { text: 'Apellido(s)', alignment: 'center' },
+                { text: 'Nombre(s)', alignment: 'center' },
+                { text: 'Correo Electrónico', alignment: 'center' },
+                { text: 'Fecha de Nacimiemto', alignment: 'center', fontSize: 7 },
+                { text: 'CURP', alignment: 'center' },
+                { text: 'NIEV', alignment: 'center' },
+              ],
+              ...buildTableBody(classroom.students.map(student => {
+                return {
+                  familyName: student.name.familyName,
+                  givenName: student.name.givenName,
+                  primaryEmail: student.primaryEmail,
+                  dob: student.dob || 'n/a',
+                  curp: student.curp || 'n/a',
+                  niev: student.niev || 'n/a',
+                }
+              }), ['familyName', 'givenName', 'primaryEmail', 'dob', 'curp', 'niev'])
+            ]
+          }
+        },
+        {
+          margin: [0, 30],
+          text: 'Lista de Materias',
+          fontSize: 14,
+          bold: false,
+          alignment: 'left',
+        },
+        {
+          fontSize: 8,
+          layout: {
+            hLineWidth: (i, node) => { return (i === 0 || i === 1 || i === -1) ? 1 : 0; },
+            vLineWidth: (i, node) => { return (i === 0 || i === 1 || i === 2 || i === 3 || i === 4 || i === 5 || i === 6) ? 1 : 0; },
+            hLineColor: (i, node) => { return (i === 0 || i === -1) ? '#101010' : '#0060a0'; },
+            vLineColor: (i, node) => { return (i === 0 || i === 1 || i === 2 || i === 3 || i === 4 || i === 5 || i === 6) ? '#080808' : '#ffffff' },
+            paddingBottom: (i, node) => {
+              switch (i) {
+                case 0:
+                  return 0;
+                case 1:
+                  return 0;
+                default:
+                  return 0;
+              }
+            },
+            paddingTop: (i, node) => {
+              switch (i) {
+                case 0:
+                  return 0;
+                case 1:
+                  return 0;
+                default:
+                  return 0;
+              }
+            }
+          },
+          table: {
+            headerRows: 0,
+            widths: ['auto', 'auto'],
+            alignment: 'center',
+            body: [
+              [
+                { text: 'Nombre de la materia', alignment: 'center' },
+                { text: 'Profesor titular', alignment: 'center' },
+
+              ],
+              ...buildTableBody(classroom.courses.map(course => {
+                return {
+                  name: course.name,
+                  teacherEmail: course.teacherEmail || 'n/a'
+                }
+              }), ['name', 'teacherEmail'])
+            ]
+          }
+        },
+      ],
+      styles: {
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 14,
+          margin: [0, 15, 0, 15]
+        },
+        tableHeader: {
+          bold: true,
+        }
+      }
+    };
+
+
+    pdfMake.createPdf(docDefinition as unknown as TDocumentDefinitions).open();
+
+
+
+
+
+  }
   notify(classroom: SchoolClassroom) {
     this.filledClassroom = { ...classroom };
     this.classroom$ = this.accountsEntityService.entities$.pipe(
